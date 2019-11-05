7ffa55232d8897ecd4c091fefb7b8e75970398ca
davidbestue <davidsanchezbestue@hotmail.com>
2019-11-05 18:47:26 +0100

    update

--- Modeling/simulations_psa.py
+++ Modeling/simulations_psa.py
@@ -1,3 +1,104 @@
 
 
-from 
+from model_psa import *
+
+numcores = multiprocessing.cpu_count()-3
+numcores
+
+
+numcores = multiprocessing.cpu_count() - 1
+separations = [3 for i in range(1)] + [3.5 for i in range(1)] + [4 for i in range(1)] + [5 for i in range(1)] +  [7 for i in range(1)] + [10 for i in range(1)] +  [12 for i in range(1)] +  [14 for i in range(1)] +  [18 for i in range(1)]
+
+
+print('1_02')
+outputs_1_02 = Parallel(n_jobs = numcores)(delayed(simulation_m_b)(totalTime=6200, 
+                                                          targ_onset = 200,
+                                                          dist_onset=550, 
+                                                          presentation_period=250,
+                                                          separation=sep, 
+                                                          inhib_curr=False, 
+                                                          time_ex_input=0, 
+                                                          sigE=1.2,
+                                                          GEE=0.016,
+                                                          plot_connectivity=False, 
+                                                          plot_dyniamic=False, 
+                                                          plot_heatmap=False, 
+                                                          plot_fit=False ) for sep in separations) 
+
+
+print('1_7')
+outputs_1_7 = Parallel(n_jobs = numcores)(delayed(simulation_m_b)(totalTime=6200, 
+                                                          targ_onset = 200,
+                                                          dist_onset=3900, 
+                                                          presentation_period=250,
+                                                          separation=sep, 
+                                                          inhib_curr=False, 
+                                                          time_ex_input=0, 
+                                                          sigE=1.2,
+                                                          GEE=0.016,
+                                                          plot_connectivity=False, 
+                                                          plot_dyniamic=False, 
+                                                          plot_heatmap=False, 
+                                                          plot_fit=False ) for sep in separations) 
+
+
+
+
+print('2_02')
+outputs_2_02 = Parallel(n_jobs = numcores)(delayed(simulation_m_b)(totalTime=6200,
+                                                          targ_onset = 550,
+                                                          dist_onset=200, 
+                                                          presentation_period=250,
+                                                          separation=sep, 
+                                                          inhib_curr=True, 
+                                                          time_ex_input=0, 
+                                                          sigE=1.2, 
+                                                          plot_connectivity=False, 
+                                                          plot_dyniamic=False, 
+                                                          plot_heatmap=False, 
+                                                          plot_fit=False ) for sep in separations) 
+
+
+
+
+print('2_7')
+outputs_2_7 = Parallel(n_jobs = numcores)(delayed(simulation_m_b)(totalTime=9600,
+                                                          targ_onset = 3950,
+                                                          dist_onset=200, 
+                                                          presentation_period=250,
+                                                          separation=sep, 
+                                                          inhib_curr=True, 
+                                                          time_ex_input=0, 
+                                                          sigE=1.2, 
+                                                          plot_connectivity=False, 
+                                                          plot_dyniamic=False, 
+                                                          plot_heatmap=False, 
+                                                          plot_fit=False ) for sep in separations) 
+
+
+
+df_1_02 = pd.DataFrame(outputs_1_02)
+df_1_02.columns= ['bias_target', 'bias_dist', 'number_of_bumps', 'angle_separation']
+df_1_02['delay']=0.2
+df_1_02['order'] =1
+
+df_1_7 = pd.DataFrame(outputs_1_7)
+df_1_7.columns= ['bias_target', 'bias_dist', 'number_of_bumps', 'angle_separation']
+df_1_7['delay']=7
+df_1_7['order'] =1
+
+df_2_02 = pd.DataFrame(outputs_2_02)
+df_2_02.columns= ['bias_target', 'bias_dist', 'number_of_bumps', 'angle_separation']
+df_2_02['delay']=0.2
+df_2_02['order'] =2
+
+df_2_7 = pd.DataFrame(outputs_2_7)
+df_2_7.columns= ['bias_target', 'bias_dist', 'number_of_bumps', 'angle_separation']
+df_2_7['delay']=7
+df_2_7['order'] =2
+
+res_simulations = pd.concat([df_1_02, df_1_7, df_2_02, df_2_7 ], ignore_index=True)
+
+
+#path_save = '/home/david/Desktop/res_sim.xlsx'
+#res_simulations.to_excel(path_save)
