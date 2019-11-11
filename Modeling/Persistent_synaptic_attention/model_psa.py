47b0e68da7ed99db4f7e8c81da2c94b152bb0eb3
davidbestue <davidsanchezbestue@hotmail.com>
2019-11-11 12:03:23 +0100

    Update model_psa.py

--- Modeling/Persistent_synaptic_attention/model_psa.py
+++ Modeling/Persistent_synaptic_attention/model_psa.py
@@ -22,7 +22,6 @@ import math
 from scipy import stats
 
 ## accesory functions
-
 def decode_rE(rE, a_ini=0, a_fin=360, N=512):
     #Population vector for a given rE
     # return ( angle in radians, absolut angle in radians, abs angle in degrees )
