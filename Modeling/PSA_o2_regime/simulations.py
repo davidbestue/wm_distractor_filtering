1df1b731e2c1a406d2698ff813c599b8d2936847
David Bestue <davidsanchezbestue@hotmail.com>
2021-01-15 14:40:54 +0100

    update

--- Modeling/PSA_o2_regime/simulations.py
+++ Modeling/PSA_o2_regime/simulations.py
@@ -1,7 +1,7 @@
 
 from model_psa_o2 import *
 
-numcores = multiprocessing.cpu_count()
+numcores = multiprocessing.cpu_count() - 3
 
 
 
@@ -21,10 +21,10 @@ order2 = distractor_onset<target_onset ##boolean
 n_times = [time_simulation for i in range(n_simuls)] 
 
 outputs_1_02_close = Parallel(n_jobs = numcores)(delayed(model1)(totalTime=t,  
-	presentation_period=presentation_period,  angle_separation=angle_separation, 
-	targ_onset=target_onset, dist_onset=distractor_onset,  order_2=order2, 
-	tauE=60, tauI=10, tauf=7000, taud=80, I0I=0.4, U=0.4, 
-	GEE=0.022, GEI=0.019, GIE=0.01 , GII=0.1, sigE=0.2, sigI=0.04, 
-	kappa_E=100, kappa_I=1.5, k_inhib=0.07, kappa_stim=50, 
-	N=512, plot_connectivity=False, plot_dyniamic=True, plot_heatmap=False, plot_fit=False, save_RE=False) for t in n_times) 
+    presentation_period=presentation_period,  angle_separation=angle_separation, 
+    targ_onset=target_onset, dist_onset=distractor_onset,  order_2=order2, 
+    tauE=60, tauI=10, tauf=7000, taud=80, I0I=0.4, U=0.4, 
+    GEE=0.022, GEI=0.019, GIE=0.01 , GII=0.1, sigE=0.2, sigI=0.04, 
+    kappa_E=100, kappa_I=1.5, k_inhib=0.07, kappa_stim=50, 
+    N=512, plot_connectivity=False, plot_dyniamic=False, plot_heatmap=False, plot_fit=False, save_RE=False) for t in n_times) 
 
