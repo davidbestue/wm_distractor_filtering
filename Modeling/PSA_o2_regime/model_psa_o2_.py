c340a730d9cfe48a1ecc5cd5c6795c25c45a474d
David Bestue <davidsanchezbestue@hotmail.com>
2021-05-18 23:44:35 +0200

    update

--- Modeling/PSA_o2_regime/model_psa_o2_.py
+++ Modeling/PSA_o2_regime/model_psa_o2_.py
@@ -263,7 +263,7 @@ def model1_(totalTime, targ_onset, dist_onset, presentation_period, angle_separa
     ## Different quadrant_selectivity options gaussian
     I0E_open =  1. #0.7 #1.2
     I0E_close= 0.5 #0.1 #0.5 
-    quadrant_selectivity_close = model_I0E_constant(I0E_close)
+    selectivity_all_close = model_I0E_constant(I0E_close)
     quadrant_selectivity_open =  model_I0E_flat( np.degrees(origin + stim_sep))*(I0E_open-I0E_close) + I0E_close
     #quadrant_selectivity_standard = model_I0E_constant(I0E_open)
     selectivity_all_open = model_I0E_constant(I0E_open) ###(I0E_open-I0E_close) + I0E_close)
@@ -272,7 +272,7 @@ def model1_(totalTime, targ_onset, dist_onset, presentation_period, angle_separa
     quadrant_selectivity = selectivity_all_open
     ##
     if order_2 == True: 
-        quadrant_selectivity = quadrant_selectivity_close  
+        quadrant_selectivity = selectivity_all_close  
     else:
         quadrant_selectivity=  selectivity_all_open  
     ##
@@ -297,14 +297,14 @@ def model1_(totalTime, targ_onset, dist_onset, presentation_period, angle_separa
         ## state depending on the time. Changes once you detect the stimulus! (add windows of time, not instantaneos)
         if order_2 == True: 
             if i<targon:
-                quadrant_selectivity = quadrant_selectivity_close #always closed for order 2 until you detect the stimui
+                quadrant_selectivity = selectivity_all_close #always closed for order 2 until you detect the stimui
             else:
                 quadrant_selectivity = selectivity_all_open #quadrant_selectivity_open ## opened just around, the rest is closed
         else: ##order 1
             if i< targon:
-                quadrant_selectivity = selectivity_all_open # all opened
+                quadrant_selectivity = selectivity_all_close # all opened
             else:
-                quadrant_selectivity = selectivity_all_open #quadrant_selectivity_open #quadrant_selectivity_open ## opened just around, the rest is closed
+                quadrant_selectivity = quadrant_selectivity_open #quadrant_selectivity_open ## opened just around, the rest is closed
 
 
 
